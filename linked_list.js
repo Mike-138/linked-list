09a72cd703722d9f021359bb2cbdbd54b6aca146
Michael <mikemcd.bbs@gmail.com>
2023-10-31 16:18:13 -0400

    Add append method to linked list

--- linked_list.js
+++ linked_list.js
@@ -1,5 +1,4 @@
-function Node() {
-    let value = null;
+function Node(value) {
     let next = null;
 
     return {
@@ -8,6 +7,41 @@ function Node() {
     }
 }
 
-function LinkedList(head) {
+function LinkedList() {
     let head = null;
+
+    function getHead() {
+        return head;
+    }
+
+    function setHead(value) {
+        head = Node(value);
+    }
+
+    function append(value) {
+        let currentNode = head;
+        let previousNode;
+
+        while (currentNode) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        
+        if (previousNode) {
+            previousNode.next = Node(value);
+        } else {
+            setHead(value);
+        }
+    }
+
+    return {
+        getHead,
+        append
+    }
 }
+
+const list = LinkedList();
+list.append(2);
+list.append(3);
+list.append(4);
+console.log(list.getHead());
